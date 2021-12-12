74e18e55f33b231a0bf4d0fc34b78dc9771f3fd5
DKER2 <pdanghuy03@gmail.com>
2021-12-12 16:10:33 +0800

    Update get_order_book fuction: Endate default = now

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from binance.client import Client
 from binance.marketData import marketData
 import tools.jsonProcess
-import json
 END_POINT = "https://api.binance.us"
 # #TRASH API
 # API_KEY= "BlauCq9caQqSBHp1txb8eR2QYbay3U9xqnzrLn6EqOo1A7D7PIFwAsw8mZL47bvj"
 # API_SIGN ="GKe8xqlfynUq3ALvK4r0V628knVD6V53Qo3dC9Jd5iczbeSLrVyKbMKx8eP5GCJw"
 # #TRASH API
 dataSoure = marketData("BTC","USDT")
-data = dataSoure.getOrderBook("1m","10/12/2021")
+data = dataSoure.getOrderBook("1d","1/12/2021")
 tools.jsonProcess.printj(data)
-tools.jsonProcess.createNewJsonDataFile("BTCUSDT")
-tools.jsonProcess.transferDataToJsonFile(data,"BTCUSDT")
+tools.jsonProcess.createNewJsonDataFile("BTCUSDT1d")
+tools.jsonProcess.transferDataToJsonFile(data,"BTCUSDT1d")
