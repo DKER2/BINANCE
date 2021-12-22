44c0131ec962aa92bd5941ea8d70abbe0bb2218e
DKER2 <pdanghuy03@gmail.com>
2021-12-22 12:53:39 +0800

    Test main.py

--- binance/marketData.py
+++ binance/marketData.py
@@ -14,7 +14,7 @@ class marketData:
     def getCandlesticks(self, interval,startDate, endDate="now"):
         """
         Get Candles Data In An Specific Interval
-        :param interval: Frequency of requesting data
+        :param interval: The interval of an candlestick ("1m","5m","1h","5h","1d","1m","1y")
         :param startDate: The start UTC+0 Human Time in type: dd/mm/yy
         :param endDate: The end Human UTC+0 Time in type: dd/mm/yy (default: now)
         :return: return text in json type
@@ -25,7 +25,7 @@ class marketData:
         else:
             tmpEndDate = str(int(time.mktime(datetime.datetime.strptime(endDate, "%d/%m/%Y").timetuple()))*1000)
         endPoint = END_POINT + "/api/v3/klines"
-        url = endPoint + "?symbol=" + self.symbol + self.eSymbol + "&" +"interval=" + interval +"&startTime=" + tmpStartDate +"&endTime=" + tmpEndDate
+        url = endPoint + "?symbol=" + self.symbol + self.eSymbol + "&" +"interval=" + interval +"&startTime=" + tmpStartDate +"&endTime=" + tmpEndDate +"&limit=1000"
         
         payload={}
         headers = {}
--- main.py
+++ main.py
@@ -1,22 +1,18 @@
+#
+#
+#   TOOLS and CLASS
 from binance.marketData import marketData
+import json
+import os
 import tools.jsonProcess
 import time
+###
+###
+###
 
-# #TRASH API
-# API_KEY= "BlauCq9caQqSBHp1txb8eR2QYbay3U9xqnzrLn6EqOo1A7D7PIFwAsw8mZL47bvj"
-# API_SIGN ="GKe8xqlfynUq3ALvK4r0V628knVD6V53Qo3dC9Jd5iczbeSLrVyKbMKx8eP5GCJw"
-# #TRASH API
-dataSoure = marketData("SOL")
-data = dataSoure.getCandlesticks("5m","13/12/2021")
-print(data)
-count = 0
-while count < 2:
-    
-    data = dataSoure.getCandlesticks("5m","13/12/2021")
-    tools.jsonProcess.createNewJsonDataFile("SOLUSDT")
-    tools.jsonProcess.transferDataToJsonFile(data,"SOLUSDT")
-    time.sleep(300)
-    count += 1
-    print(count)
-    
-print(count)
+
+datasource  = marketData("ETH")
+data = datasource.getCandlesticks("1m","20/12/2021","22/12/2021")
+tools.jsonProcess.createNewJsonDataFile("ETHUSD")
+tools.jsonProcess.transferDataToJsonFile(data,"ETHUSD")
+
